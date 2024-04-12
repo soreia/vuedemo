9e0b333d9f414ff126bc460d45a700cbfe0ec79f
liya.su <suliyaa@126.com>
2024-04-12 17:47:23 +0800

    添加post方法

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'; // 导入 App 组件
 import routes from './router'; // 导入路由配置
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import ApiService from './js/common/apiService';
 
 Vue.use(VueRouter);
 Vue.use(ElementUI);
+Vue.prototype.$apiService = ApiService;  // 将 API 服务添加到 Vue 原型
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
--- src/views/components/phoneDialog.vue
+++ src/views/components/phoneDialog.vue
@@ -4,7 +4,6 @@
     title="电话通信"
     width="30%"
     :visible="localDialogVisible"
-    @close="closeDialog"
   >
     <div>免费回电</div>
     <div>请放心接听 服务时间全天24小时</div>
@@ -12,6 +11,8 @@
     <el-input style='width: 70%;' v-model="input2" placeholder="请输入短信验证码">
     </el-input>
     <el-button icon="el-icon-search" style='width: 30%;'>验证码</el-button>
+    <el-input  type="textarea" v-model="input3" placeholder="请输入咨询内容">
+    </el-input>
     <span class="dialog-footer">
       <el-button @click="closeDialog">免费回电</el-button>
     </span>
@@ -27,6 +28,7 @@
       return {
         input1: '',
         input2: '',
+        input3: '',
         localDialogVisible: false,  // 创建一个新的数据属性
       };
     },
@@ -39,9 +41,27 @@
   },
     methods: {
     closeDialog() {
+    console.log('this is Dialog ----closeDialog');
+    this.savePhoneMessage();
     this.localDialogVisible = false; 
     this.$emit('updatePhoneDialogVisible', this.localDialogVisible); 
   },
+  savePhoneMessage(){
+    this.$apiService.post('http://localhost:8091/api/savePhoneMessage', {
+      phoneNum: this.input1,
+      phoneMessage: this.input3
+    }).then(response => {
+       return response.data;
+      // handle response
+    }).catch(error => {
+      // handle error
+      console.log(error);
+      this.submitStatus =false;
+    });
+
+
+  }
+
 },
   }
 </script>
